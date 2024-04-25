7920f62c4fd5565d3da16df9489f726fd28b50cf
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-04-25 18:03:15 +0300

    Added Slider in the Films

---
+++ src/components/Films/Films.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import { Slider } from './components';
+
+export const Films = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const apiKey = 'bc8eebf42f936c16863715b5622480d4';
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ru-RU`);
+        const data = await response.json();
+        setMovies(data.results);
+        console.log(data.results);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      <h1>Лучшие фильмы</h1>
+      {/* <ul className="movie-list">
+        {movies.map(movie => (
+          <li key={movie.id} className="movie">
+            <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.title} />
+            <h2>{movie.title}</h2>
+            <p>{movie.overview}</p>
+          </li>
+        ))}
+      </ul> */}
+      <Slider movies={movies} />
+    </>
+  )
+};
