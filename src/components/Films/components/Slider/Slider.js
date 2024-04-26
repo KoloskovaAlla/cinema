372278234914f191fe6596f1ebbf56d5be40ad6d
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-04-26 16:35:06 +0300

    Added slider navigation

--- src/components/Films/components/Slider/Slider.js
+++ src/components/Films/components/Slider/Slider.js
@@ -1,27 +1,36 @@
 
 import classes from './Slider.module.scss';
+import { useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-
 import 'swiper/css';
+import { LeftArrowIcon, RightArrowIcon } from './assets';
 
 export const Slider = ({ movies }) => {
+  const navigationPrevRef = useRef(null);
+  const navigationNextRef = useRef(null);
+
   return (
     <div className={classes.wrapper}>
       <Swiper
+        className={classes.mySwiper}
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={20}
         slidesPerView={3}
         breakpoints={{
           770: {
-            slidesPerView: 3
+            slidesPerView: 4
           },
           1150: {
-            slidesPerView: 4
+            slidesPerView: 5
           },
           1440: {
-            slidesPerView: 4
+            slidesPerView: 6
           }
+        }}
+        navigation={{
+          nextEl: navigationNextRef.current,
+          prevEl: navigationPrevRef.current,
         }}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
@@ -36,7 +45,12 @@ export const Slider = ({ movies }) => {
             </div>
           </SwiperSlide>
         ))}
-
+        <button className={classes.swiper_button_prev_custom} ref={navigationPrevRef}>
+          <LeftArrowIcon />
+        </button>
+        <button className={classes.swiper_button_next_custom} ref={navigationNextRef}>
+          <RightArrowIcon />
+        </button>
 
       </Swiper>
     </div>
---
+++ src/components/Films/components/Slider/assets/index.js
@@ -0,0 +1,3 @@
+export { ReactComponent as LeftArrowIcon } from './arrow_left.svg';
+// export { ReactComponent as LeftArrowIcon } from './Arrow_left_long_light.svg';
+export { ReactComponent as RightArrowIcon } from './arrow_right.svg';
