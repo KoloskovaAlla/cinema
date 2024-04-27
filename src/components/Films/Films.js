ba0f5dc74b64d90628d6d7c1ed03b348f953dca7
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-04-27 15:27:17 +0300

    Restructuring Films components

--- src/components/Films/Films.js
+++ src/components/Films/Films.js
@@ -1,29 +1,60 @@
-import { useState, useEffect } from 'react';
-import { Slider } from './components';
 
-export const Films = () => {
-  const [movies, setMovies] = useState([]);
+import classes from './Films.module.scss';
+import { useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import { LeftArrowIcon, RightArrowIcon } from './assets';
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const apiKey = 'bc8eebf42f936c16863715b5622480d4';
-        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=ru-RU`);
-        const data = await response.json();
-        setMovies(data.results);
-        console.log(data.results);
-      } catch (error) {
-        console.error('Error fetching movies:', error);
-      }
-    };
-
-    fetchMovies();
-  }, []);
+export const Films = ({ movies }) => {
+  const navigationPrevRef = useRef(null);
+  const navigationNextRef = useRef(null);
 
   return (
-    <>
+    <div className={classes.wrapper}>
       <h1>Лучшие фильмы</h1>
-      <Slider movies={movies} />
-    </>
-  )
+      <Swiper
+        className={classes.mySwiper}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={10}
+        slidesPerView={1}
+        breakpoints={{
+          770: {
+            slidesPerView: 2
+          },
+          1150: {
+            slidesPerView: 3
+          },
+          1440: {
+            slidesPerView: 4
+          }
+        }}
+        navigation={{
+          nextEl: navigationNextRef.current,
+          prevEl: navigationPrevRef.current,
+        }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+
+        {movies.map((movie, index) => (
+          <SwiperSlide key={index}>
+            <div key={movie.id} className="movie">
+              <div className={classes.image}>
+                <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.title} />
+              </div>
+              <h2>{movie.title}</h2>
+            </div>
+          </SwiperSlide>
+        ))}
+        <button className={classes.swiper_button_prev_custom} ref={navigationPrevRef}>
+          <LeftArrowIcon />
+        </button>
+        <button className={classes.swiper_button_next_custom} ref={navigationNextRef}>
+          <RightArrowIcon />
+        </button>
+
+      </Swiper>
+    </div>
+  );
 };
