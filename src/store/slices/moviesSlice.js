a21908d2ef1c08beaa05f7608353c8be557cc8c1
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2024-08-10 14:37:17 +0300

    Added genre to database

--- src/store/slices/moviesSlice.js
+++ src/store/slices/moviesSlice.js
@@ -7,8 +7,18 @@ const onGetMovies = async (_, thunkAPI) => {
     const url = `https://www.omdbapi.com/?s=movie&apikey=${apiKey}`;
     const response = await fetch(url);
     const data = await response.json();
+
     if (!data.Search) throw new Error(data.message);
-    return thunkAPI.fulfillWithValue(data.Search);
+     // Получаем жанр для каждого фильма
+    const moviesWithGenre = await Promise.all(
+      data.Search.map(async (movie) => {
+        const movieDetailsUrl = `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${apiKey}`;
+        const movieDetailsResponse = await fetch(movieDetailsUrl);
+        const movieDetails = await movieDetailsResponse.json();
+        return { ...movie, genre: movieDetails.genre }; // Добавляем жанр
+      })
+    );
+    return thunkAPI.fulfillWithValue(moviesWithGenre);
   } catch (error) {
     const { message } = error;
     console.error(message);
