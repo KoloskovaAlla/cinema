f5e8250a8807a8414c8d62783af96156617d1d47
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-08-26 12:23:47 +0300

    Changed styles of the buttons in the Similar Films

--- src/widgets/SimilarFilms/assets/index.js
+++ src/widgets/SimilarFilms/assets/index.js
@@ -1,3 +1,5 @@
 
-export { ReactComponent as LeftArrowIcon } from './arrow_left.svg';
-export { ReactComponent as RightArrowIcon } from './arrow_right.svg';
+// export { ReactComponent as LeftArrowIcon } from './arrow_left.svg';
+export { ReactComponent as LeftArrowIcon } from './Expand_left.svg';
+// export { ReactComponent as RightArrowIcon } from './arrow_right.svg';
+export { ReactComponent as RightArrowIcon } from './Expand_right.svg';
