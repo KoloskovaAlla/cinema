277f1aef88df55dc773c8f34609486d291a2c575
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-09-06 17:05:52 +0300

    Moved Header to the widgets

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,14 +1,14 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { HomePage } from './HomePage';
 import { FilmPage } from './FilmPage/FilmPage';
-import { Header } from 'components';
+import { Header } from 'widgets';
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />      
+        <Route path="/" element={<HomePage />} />
         <Route path="/:id" element={<FilmPage />} />
       </Routes>
     </BrowserRouter>
