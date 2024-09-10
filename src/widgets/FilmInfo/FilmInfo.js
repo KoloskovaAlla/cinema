c979f83c607236d8dce7f1ffe3bc3bc6faf6ae38
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-09-10 11:59:59 +0300

    Changed styles in the FilmInfo for a large screen

--- src/widgets/FilmInfo/FilmInfo.js
+++ src/widgets/FilmInfo/FilmInfo.js
@@ -17,17 +17,84 @@ export const FilmInfo = ({ film }) => {
             {film?.Title}
           </h1>
 
-
-
           <div className={classes.infoBlock}>
-            <p>{film?.genre}</p>
-            <p>{`Year: ${film?.Year}`}</p>
-            <p>{`Runtime: ${film?.runtime}`}</p>
-            <p>{`Country: ${film?.country}`}</p>
-            <p>{`Rating: ${film?.rating}`}</p>
-            <p>{`Actors: ${film?.actors}`}</p>
-            <p>{`Director: ${film?.director}`}</p>
-            <p>{`Writer: ${film?.writer}`}</p>
+            <div className={classes.infoItem}>
+              <div className={classes.infoLabel}>
+                Genre
+              </div>
+              <div className={classes.infoValue}>
+                {film?.genre}
+              </div>
+            </div>
+            <div className={classes.infoItem}>
+              <div className={classes.infoLabel}>
+                Year
+              </div>
+              <div className={classes.infoValue}>
+                {film?.Year}
+              </div>
+            </div>
+            <div className={classes.infoItem}>
+              <div className={classes.infoLabel}>
+                Runtime
+              </div>
+              <div className={classes.infoValue}>
+                {film?.runtime}
+              </div>
+            </div>
+
+            <div className={classes.infoItem}>
+              <div className={classes.infoLabel}>
+                Country
+              </div>
+              <div className={classes.infoValue}>
+                {film?.country}
+              </div>
+            </div>
+
+            <div className={classes.infoItem}>
+              <div className={classes.infoLabel}>
+                Rating
+              </div>
+              <div className={classes.infoValue}>
+                {film?.rating}
+              </div>
+            </div>
+
+            <div className={classes.infoItem}>
+              <div className={classes.infoLabel}>
+                Actors
+              </div>
+              <div className={classes.infoValue}>
+                {film?.actors}
+              </div>
+            </div>
+
+            <div className={classes.infoItem}>
+              <div className={classes.infoLabel}>
+                Director
+              </div>
+              <div className={classes.infoValue}>
+                {film?.director}
+              </div>
+            </div>
+            <div className={classes.infoItem}>
+              <div className={classes.infoLabel}>
+                Writer
+              </div>
+              <div className={classes.infoValue}>
+                {film?.writer}
+              </div>
+            </div>
+
+            {/* <p>{`Genre: ${film?.genre}`}</p>
+            <p>{`Year: ${film?.Year}`}</p> */}
+            {/* <p>{`Runtime: ${film?.runtime}`}</p> */}
+            {/* <p>{`Country: ${film?.country}`}</p> */}
+            {/* <p>{`Rating: ${film?.rating}`}</p> */}
+            {/* <p>{`Actors: ${film?.actors}`}</p> */}
+            {/* <p>{`Director: ${film?.director}`}</p> */}
+            {/* <p>{`Writer: ${film?.writer}`}</p> */}
           </div>
           <p className={classes.copy}>{film?.plot}</p>
         </div>
