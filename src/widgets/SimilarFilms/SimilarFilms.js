887304d17d40e195e41e9558b3a8c58d9a7151cd
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-10-14 11:26:17 +0300

    Made changing the button on a touch event

--- src/widgets/SimilarFilms/SimilarFilms.js
+++ src/widgets/SimilarFilms/SimilarFilms.js
@@ -34,6 +34,80 @@ export const SimilarFilms = ({ similarFilms }) => {
   const moviesState = useMovies();
   const { movies } = moviesState;
 
+  useEffect(() => {
+    const prevButton = navigationPrevSimilarRef.current;
+    const nextButton = navigationNextSimilarRef.current;
+
+    if (prevButton && nextButton) {
+      const handlePrevTouchStart = () => {
+        console.log('Prev button clicked');
+        console.log(prevButton);
+
+        // Изменяем масштаб кнопки
+        prevButton.style.transform = 'scale(1.5)'; // Увеличиваем размер кнопки
+
+        // Возвращаем размер к нормальному через 300 мс
+        // setTimeout(() => {
+        //   prevButton.style.transform = 'scale(1)'; // Возвращаем к нормальному размеру
+        // }, 300);
+      };
+
+      const handlePrevTouchEnd = () => {
+        console.log('Prev button clicked');
+        console.log(prevButton);
+
+        // Изменяем масштаб кнопки
+        prevButton.style.transform = 'scale(1.0)'; // Увеличиваем размер кнопки
+
+        // Возвращаем размер к нормальному через 300 мс
+        // setTimeout(() => {
+        //   prevButton.style.transform = 'scale(1)'; // Возвращаем к нормальному размеру
+        // }, 300);
+      };
+
+      const handleNextTouchStart = () => {
+        console.log('Next button clicked');
+        console.log(nextButton);
+
+        // Изменяем масштаб кнопки
+        nextButton.style.transform = 'scale(1.5)'; // Увеличиваем размер кнопки
+
+        // Возвращаем размер к нормальному через 300 мс
+        // setTimeout(() => {
+        //   nextButton.style.transform = 'scale(1)'; // Возвращаем к нормальному размеру
+        // }, 300);
+      };
+
+      const handleNextTouchEnd = () => {
+        console.log('Next button clicked');
+        console.log(nextButton);
+
+        // Изменяем масштаб кнопки
+        nextButton.style.transform = 'scale(1.0)'; // Увеличиваем размер кнопки
+
+        // Возвращаем размер к нормальному через 300 мс
+        // setTimeout(() => {
+        //   nextButton.style.transform = 'scale(1)'; // Возвращаем к нормальному размеру
+        // }, 300);
+      };
+
+
+      // Добавляем обработчики
+      prevButton.addEventListener('touchstart', handlePrevTouchStart);
+      prevButton.addEventListener('touchend', handlePrevTouchEnd);
+
+      nextButton.addEventListener('touchstart', handleNextTouchStart);
+      nextButton.addEventListener('touchend', handleNextTouchEnd);
+      // Убираем обработчики при размонтировании
+      // return () => {
+      //   prevButton.removeEventListener('click', handlePrevClick);
+      //   nextButton.removeEventListener('click', handleNextClick);
+      // };
+    } else {
+      console.log("Buttons not ready yet");
+    }
+  }, [navigationPrevSimilarRef, navigationNextSimilarRef, movies]); // Добавляем movies как зависимость
+
 
   if (!film) return;
   return (
