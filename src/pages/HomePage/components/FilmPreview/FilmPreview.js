4e6111a89099cae7917eff6428c7889a13e41396
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-10-31 13:11:43 +0300

    Refactored FilmPreview

--- src/pages/HomePage/components/FilmPreview/FilmPreview.js
+++ src/pages/HomePage/components/FilmPreview/FilmPreview.js
@@ -1,7 +1,6 @@
-import React from 'react';
 import classes from './FilmPreview.module.scss';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import {useEffect} from 'react';
 
 export const FilmPreview = ({ movie }) => {
   const { imdbID, Title, Poster } = movie;
--- src/widgets/SimilarFilms/components/FilmPreview/FilmPreview.js
+++ src/widgets/SimilarFilms/components/FilmPreview/FilmPreview.js
@@ -1,22 +1,18 @@
 import classes from './FilmPreview.module.scss';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { useFilm } from 'shared/hooks';
-import {useEffect} from 'react';
+
 
 export const FilmPreview = ({ movie }) => {
-  const dispatch = useDispatch();
   const { imdbID, Title, Poster } = movie;
 
   // Это в секции похожих фильмов
-  return (  
-     <Link to={`/${imdbID}`}>
-    {/* <Link to={`/${movie.id}`} onClick={handlePreviewClick}> */}
+  return (
+    <Link to={`/${imdbID}`}>
       <div key={movie.id} className="movie">
         <div className={classes.image}>
-          <img src={movie.Poster} alt={movie.title} />
+          <img src={Poster} alt={Title} />
         </div>
-        <h2>{movie.title}</h2>
+        <h2>{Title}</h2>
       </div>
     </Link>
   );
