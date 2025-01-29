e3cddb357835a204a0891d6e2e14851bb069f6fb
Alla <polyglotdreamaboutit@gmail.com>
2025-01-29 09:32:42 +0300

    Added dispatch in the SeriesPage

--- src/pages/SeriesPage/SeriesPage.js
+++ src/pages/SeriesPage/SeriesPage.js
@@ -1,5 +1,20 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSeries } from 'shared/hooks';
+
 export const SeriesPage = () => {
+    const { state, getSeries } = useSeries();
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getSeries());
+    }, [dispatch]);
+
+    // console.log(state);
+    
+
     return (
         <div>Seriespage</div>
     )
-};
+}
