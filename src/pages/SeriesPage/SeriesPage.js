442c10dd8fc1e7d862ae1b252ef2bf04e743b311
Alla <polyglotdreamaboutit@gmail.com>
2025-02-06 09:26:02 +0300

    Added components of the Slider

--- src/pages/SeriesPage/SeriesPage.js
+++ src/pages/SeriesPage/SeriesPage.js
@@ -1,15 +1,29 @@
-import { useEffect } from 'react';
+import classes from './SeriesPage.module.scss';
+import { useRef, useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSeries } from 'shared/hooks';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { SeriePreview } from './components';
+import { classNames } from 'shared/utils/helpers';
 
 export const SeriesPage = () => {
     const dispatch = useDispatch();
+    const navigationNextRef = useRef(null);
 
     const seriesState = useSeries();
     const { series } = seriesState;
+
+    const [isNextDisabled, setIsNextDisabled] = useState(false);
+    const buttonNextClassNames = classNames(classes.swiper_button_next_custom, {
+        [classes.disableNext]: isNextDisabled,
+      });
+
+
+      const onSlideChange = (swiper) => {
+        // setIsPrevDisabled(swiper.isBeginning);
+        setIsNextDisabled(swiper.isEnd);
+      };
 
     useEffect(() => {
         dispatch(seriesState.getSeries());
@@ -44,10 +58,10 @@ export const SeriesPage = () => {
                 slidesPerView: 5
                 }
             }}
-            // navigation={{
-            //     nextEl: navigationNextRef.current,
-            //     prevEl: navigationPrevRef.current,
-            // }}
+            navigation={{
+                nextEl: navigationNextRef.current,
+                // prevEl: navigationPrevRef.current,
+            }}
             // pagination={{
             //     el: paginationRef.current,
             //     clickable: true,
@@ -68,15 +82,15 @@ export const SeriesPage = () => {
                 ref={navigationPrevRef}
             >
                 <LeftArrowIcon />
-            </button>
-            <div className={classes.swiper_pagination_custom} ref={paginationRef}>
-            </div>
+            </button> */}
+            {/* <div className={classes.swiper_pagination_custom} ref={paginationRef}>
+            </div> */}
             <button
                 className={buttonNextClassNames}
                 ref={navigationNextRef}
             >
-                <RightArrowIcon />
-            </button> */}
+                {/* <RightArrowIcon /> */}
+            </button>
         </Swiper>
         </div>
     )
--- src/pages/SeriesPage/components/SeriePreview/SeriePreview.js
+++ src/pages/SeriesPage/components/SeriePreview/SeriePreview.js
@@ -1,8 +1,46 @@
+import classes from './SeriePreview.module.scss';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 export const SeriePreview = ({ serie }) => {
+    const { imdbID, Title, Poster } = serie;
+
+    useEffect(() => {
+        console.log(serie);
+    }, [serie]);
+
+    const handleMouseEnter = () => {   
+        // setIsHovered(true);
+        // imageRef.current.style.scale = '1.2';
+        // imageRef.current.style.transitionProperty = 'all';
+        // imageRef.current.style.transitionDuration = '1s';     
+        // console.log('mouse entered')
+      };
+      
+      const handleMouseLeave = () => {
+        // setIsHovered(false);
+        // imageRef.current.style.scale = '1';
+        // imageRef.current.style.transitionProperty = 'all';
+        console.log('mouse left')
+      };
+
     return (
-        <div>
-            превью сериала
-            {serie.Title}
+        <Link to={`/${imdbID}`}        
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}              
+        >
+        <div key={imdbID} className={classes.serie}
+          // className={isHovered ? classes.hovered : ''}      
+        >
+          <div className={classes.image}>
+            <img 
+              src={Poster} 
+              alt={Title} 
+            //   ref={imageRef}
+            />
+          </div>
+          <h2 className={classes.title}>{Title}</h2>
         </div>
+      </Link>
     )
 };
