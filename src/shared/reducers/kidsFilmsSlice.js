8d68b82b41f59d3101348da5c51b70b166dd6aec
Alla <polyglotdreamaboutit@gmail.com>
2025-04-19 19:03:40 +0300

    Refactor kidsFilmsSlice

--- src/shared/reducers/kidsFilmsSlice.js
+++ src/shared/reducers/kidsFilmsSlice.js
@@ -1,87 +1,81 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { API_KEY } from 'shared/constants/api';
 
+// Категории детских фильмов
+const categories = [
+  // Общие термины
+  'children',
+
+  // Жанры
+  'adventure', 'fairy',
+
+  // Персонажи
+  'princess', 'animal', 'dog', 'pony', 'dinosaur', 'puppy', 'kitten',
+
+  // Эмоции и предметы
+  'friends', 'toy', 'toys',
+
+  // Сказочные элементы
+  'wonder', 'world',
+
+  // Природа
+  'space', 'jungle',
+];
+
+// Получить список фильмов по категории
+const fetchFilmsByCategory = async (category) => {
+  const url = `https://www.omdbapi.com/?s=${category}&apikey=${API_KEY}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.Search || [];
+};
+
+// Получить подробности по одному фильму
+const fetchFilmDetails = async (imdbID) => {
+  const url = `https://www.omdbapi.com/?i=${imdbID}&apikey=${API_KEY}`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return {
+    genre: data.Genre || 'N/A',
+    year: data.Year || 'N/A',
+    runtime: data.Runtime || 'N/A',
+    country: data.Country || 'N/A',
+    rating: data.imdbRating || 'N/A',
+    actors: data.Actors || 'N/A',
+    director: data.Director || 'N/A',
+    writer: data.Writer || 'N/A',
+    plot: data.Plot || 'N/A',
+  };
+};
+
+// Основной thunk
 const onGetKidsFilms = async (_, thunkAPI) => {
   try {
-    // const apiKey = '35b2affc';
-    console.log('API_KEY импортированный:', API_KEY);
-    const  categories = [
-      // Общие детские термины
-      'children',
-      
-      // // Жанры детского контента
-      'adventure', 'fairy',
-      
-      // // Персонажи и существа
-      'princess', 'animal', 'dog', 'pony', 'dinosaur', 'puppy', 'kitten',
-      
-      // // Распространенные слова в детских названиях  
-      
-      // // Позитивные эмоции и темы
-      'friends', 'toy', 'toys',
-      
-      // // Сказочные элементы
-      'wonder','world',
-      
-      // // Природа и времена года
-      'space', 'jungle',
-    ];
-   
-    let kidsFilms = [];
-    
-    for (const category of categories) {
-      // const url = `https://www.omdbapi.com/?s=${category}&apikey=${apiKey}`;
-      const url = `https://www.omdbapi.com/?s=${category}&apikey=${API_KEY}`;
-      const response = await fetch(url);
-      const data = await response.json();
+    const filmsByCategory = await Promise.all(categories.map(fetchFilmsByCategory));
+    const allFilms = filmsByCategory.flat();
+    const shuffled = allFilms.sort(() => Math.random() - 0.5).slice(0, 20);
 
-      if (data.Search) {
-        kidsFilms.push(...data.Search);
-      }
-    }
-
-    // Перемешиваем фильмы
-    kidsFilms.sort(() => Math.random() - 0.5);
-
-    // Запрашиваем детали для каждого фильма
-    const kidsFilmsWithDetails = await Promise.all(
-        kidsFilms.slice(0, 20).map(async (kidsFilm) => { // Ограничение для избежания лимитов API
-        // const kidsFilmDetailsUrl = `https://www.omdbapi.com/?i=${kidsFilm.imdbID}&apikey=${apiKey}`;
-        const kidsFilmDetailsUrl = `https://www.omdbapi.com/?i=${kidsFilm.imdbID}&apikey=${API_KEY}`;
-        const kidsFilmDetailsResponse = await fetch(kidsFilmDetailsUrl);
-        const kidsFilmDetails = await kidsFilmDetailsResponse.json();         
-
-        return {
-          ...kidsFilm,
-          genre: kidsFilmDetails.Genre || 'N/A',
-          year: kidsFilmDetails.Year || 'N/A',
-          runtime: kidsFilmDetails.Runtime || 'N/A',
-          country: kidsFilmDetails.Country || 'N/A',
-          rating: kidsFilmDetails.imdbRating || 'N/A',
-          actors: kidsFilmDetails.Actors || 'N/A',
-          director: kidsFilmDetails.Director || 'N/A',
-          writer: kidsFilmDetails.Writer || 'N/A',
-          plot: kidsFilmDetails.Plot || 'N/A',
-        };
-      })
+    const detailedFilms = await Promise.all(
+      shuffled.map(async (film) => ({
+        ...film,
+        ...(await fetchFilmDetails(film.imdbID)),
+      }))
     );
 
-    return thunkAPI.fulfillWithValue(kidsFilmsWithDetails);
+    return thunkAPI.fulfillWithValue(detailedFilms);
   } catch (error) {
     console.error(error.message);
     return thunkAPI.rejectWithValue(error.message);
   }
 };
 
-const getKidsFilms = createAsyncThunk(
-  'movies/getKidsFilms',
-  onGetKidsFilms,
-);
+const getKidsFilms = createAsyncThunk('movies/getKidsFilms', onGetKidsFilms);
 
 const initialState = {
-  isLoadingMovies: false,
+  isLoadingKidsFilms: false,
   kidsFilms: null,
-  errorMessageMovies: '',
+  errorKidsFilms: '',
 };
 
 const kidsFilmsSlice = createSlice({
@@ -91,19 +85,19 @@ const kidsFilmsSlice = createSlice({
   extraReducers: builder => {
     builder
       .addCase(getKidsFilms.pending, state => {
-        state.isLoadingMovies = true;
+        state.isLoadingKidsFilms = true;
         state.kidsFilms = null;
-        state.errorMessageMovies = '';
+        state.errorKidsFilms = '';
       })
       .addCase(getKidsFilms.fulfilled, (state, { payload }) => {
-        state.isLoadingMovies = false;
+        state.isLoadingKidsFilms = false;
         state.kidsFilms = payload;
-        state.errorMessageMovies = '';
+        state.errorKidsFilms = '';
       })
       .addCase(getKidsFilms.rejected, (state, { payload }) => {
-        state.isLoadingMovies = false;
+        state.isLoadingKidsFilms = false;
         state.kidsFilms = null;
-        state.errorMessageMovies = payload;
+        state.errorKidsFilms = payload;
       });
   }
 });
